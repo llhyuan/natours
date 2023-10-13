9e0c9c1fe06ca5195883df7414e903ec3c627751
llhyuan <lhyuan.liu21@icloud.com>
2023-10-13 12:09:00 +0200

    Removed jamming animation.

--- src/components/TopTrips.tsx
+++ src/components/TopTrips.tsx
@@ -1,12 +1,6 @@
-"use client";
 import InfoCard from "@/components/InfoCard";
 import { Tour } from "@Global/custom-types";
 import { Lato } from "next/font/google";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { useEffect, useRef } from "react";
-
-gsap.registerPlugin(ScrollTrigger);
 
 const latoBold = Lato({
   weight: "700",
@@ -15,36 +9,13 @@ const latoBold = Lato({
 });
 
 export default function TopTrips({ tours }: { tours: Array<Tour> }) {
-  const topTripsRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: topTripsRef.current,
-        scrub: true,
-        pin: true,
-        start: "top 100px",
-      },
-    });
-    const viewportWidth = window.innerWidth;
-    const topTripsWidth = 2200;
-
-    tl.fromTo(
-      topTripsRef.current,
-      { x: 0 },
-      { x: -(topTripsWidth - viewportWidth) },
-    );
-  });
   return (
     <div>
       <h2 className={latoBold.className + " text-2xl md:text-3xl text-center"}>
         Top trips for 2024
       </h2>
       <div className="w-[100vw] overflow-scroll s bg-transparent snap-mandatory">
-        <div
-          ref={topTripsRef}
-          className="flex justify-around py-8 w-[2200px] lg:px-[15vw]"
-        >
+        <div className="flex justify-around py-8 w-[2200px] lg:px-[15vw]">
           {tours.map((tour: any, index: number) => {
             return (
               <div
